0ad1913a08c578c9949c3a1a979457cd8c548690
Carol Willing <carolcode@willingconsulting.com>
2019-05-23 12:11:55 -0700

    prettier theme

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 import recommonmark
 from recommonmark.parser import CommonMarkParser
+import sphinx_bootstrap_theme
 
 # -- General configuration ------------------------------------------------
 
@@ -95,7 +96,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -122,7 +124,9 @@ html_static_path = ['_static']
 #
 # This is required for the alabaster theme
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
-html_sidebars = {'**': ['about.html', 'navigation.html', 'relations.html', 'searchbox.html']}
+html_sidebars = {
+    '**': ['about.html', 'navigation.html', 'relations.html', 'searchbox.html']
+}
 
 
 # -- Options for HTMLHelp output ------------------------------------------
